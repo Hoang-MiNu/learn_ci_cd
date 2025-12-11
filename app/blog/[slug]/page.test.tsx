bb97006042061cc5f87a55c3d5e4edec46da59ff
Minh Nhựt(MiNu) <67326304+nhut2409@users.noreply.github.com>
2025-12-11 15:52:29 +0700

    finally not error
    * udpate
    
    * udpate
    
    * udpate
    
    * udpate
    
    * udpate
    
    ---------
    
    Co-authored-by: NhutHM <nhuthm@aipower.com.vn>

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -1,10 +1,18 @@
 import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import Page from "./page";
 
 test("App Router: Works with dynamic route segments", () => {
   render(<Page params={{ slug: "Test" }} />);
   expect(
     screen.getByRole("heading", { level: 1, name: "Slug: Test" }),
+  ).toBeDefined();
+  cleanup();
+});
+
+test("Hoàng MiNu should be in page", () => {
+  render(<Page params={{ slug: "Hoàng MiNu" }} />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Slug: Hoàng MiNu" }),
   ).toBeDefined();
 });
