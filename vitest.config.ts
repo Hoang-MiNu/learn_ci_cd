4225791343cd15a8aaa1541f9a09285b389a6bbb
Minh Nhựt(MiNu) <67326304+nhut2409@users.noreply.github.com>
2025-12-11 16:27:32 +0700

    fix bug build error (#3)
    * udpate
    
    * udpate
    
    * udpate
    
    * udpate
    
    * udpate
    
    * udpate
    
    * udpate
    
    ---------
    
    Co-authored-by: NhutHM <nhuthm@aipower.com.vn>

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    exclude: ['**/node_modules/**', '**/build/**'],
+    coverage: {
+      exclude: ['**/build/**', 'next.config.ts', 'vitest.config.ts', '.next/**']
+    }
   },
 });
